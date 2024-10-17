9d360cf9babd6cbddd78597b288b07d6ca06d666
Pankaj <pankaj.awasthi@novoinvent.com>
2024-10-17 17:53:06 +0530

    update query UI

---
+++ app/src/main/java/com/example/compose_libraryapp/screens/UpdateScreen.kt
@@ -0,0 +1,30 @@
+package com.example.compose_libraryapp.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.compose_libraryapp.viewmodel.BookViewModel
+
+@Composable
+fun UpdateScreen(viewModel: BookViewModel, bookId: String?) {
+    Column() {
+        var inputBook by remember {
+            mutableStateOf("")
+        }
+
+        OutlinedTextField(value = inputBook,
+            onValueChange = { enteredValue -> inputBook = enteredValue },
+            label = { Text(text = "Update Book Name") },
+            placeholder = { Text(text = "New Book Name") }
+        )
+        Button(onClick = {  }) {
+            
+        }
+    }
+}
